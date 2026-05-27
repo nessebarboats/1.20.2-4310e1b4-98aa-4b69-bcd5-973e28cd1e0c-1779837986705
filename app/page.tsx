a88353fff149e8ef572f2ac9f2d630748fcf97ef
nessebarboats <nessebarboats@gmail.com>
2026-05-28 02:37:21 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -40,18 +40,6 @@ import {
 
 
 
-export default function AspectRatioDemo() {
-  return (
-    <AspectRatio ratio={16 / 9} className="bg-muted">
-      <Image
-        src="https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80"
-        alt="Photo by Drew Beamer"
-        fill
-        className="h-full w-full rounded-md object-cover"
-      />
-    </AspectRatio>
-  )
-}
 
 
 export default function Home() {
@@ -93,7 +81,14 @@ export default function Home() {
       />
 
 
-
+    <AspectRatio ratio={16 / 9} className="bg-muted">
+      <Image
+        src="https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80"
+        alt="Photo by Drew Beamer"
+        fill
+        className="h-full w-full rounded-md object-cover"
+      />
+    </AspectRatio>
 
       
       <LandingPrimaryImageCtaSection

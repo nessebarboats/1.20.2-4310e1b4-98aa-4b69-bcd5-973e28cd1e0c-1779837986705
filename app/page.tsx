b357635baabab8854d0f9a3c991f3826eb290b09
nessebarboats <nessebarboats@gmail.com>
2026-06-16 00:58:52 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -20,6 +20,9 @@ import { LandingSocialProofBand } from '@/components/landing/social-proof/Landin
 import { LandingSocialProofBandItem } from '@/components/landing/social-proof/LandingSocialProofBandItem';
 import { LandingProductVideoFeature } from '@/components/landing/LandingProductVideoFeature';
 
+import { LandingPricingSection } from '@/components/landing/pricing/LandingPricingSection';
+import { LandingPricingPlan } from '@/components/landing/pricing/LandingPricingPlan';
+
 import Image from "next/image"
 
 
@@ -127,6 +130,7 @@ export default function Home() {
   </div>
 </a>
 </section>
+      
 <LandingProductFeature
         title="Customized Boat Tours"
         descriptionComponent={
@@ -166,7 +170,48 @@ export default function Home() {
         imagePerspective="none"
       />
 
- 
+
+<LandingPricingSection
+  title="Simple, scalable pricing"
+  description="Affordable pricing plans tailored to your needs. Choose a plan that works best for you."
+>
+  <LandingPricingPlan
+    title="Free"
+    description="For small teams & personal use."
+    price="$0"
+  >
+    <p>Up to 5 users</p>
+    <p>Basic features</p>
+    <p>Discord access</p>
+  </LandingPricingPlan>
+
+  <LandingPricingPlan
+    title="Pro"
+    description="For larger teams or businesses."
+    ctaText="Upgrade"
+    price="$20"
+    priceSuffix="/mo"
+    highlighted
+  >
+    <p>Unlimited users</p>
+    <p>AI features</p>
+    <p>Priority support</p>
+  </LandingPricingPlan>
+
+  <LandingPricingPlan
+    title="Enterprise"
+    description="For enterprise teams & businesses."
+    ctaText="Sign up"
+    price="$100"
+    priceSuffix="/mo"
+    featured
+  >
+    <p>Unlimited users</p>
+    <p>AI features</p>
+    <p>Priority support</p>
+  </LandingPricingPlan>
+</LandingPricingSection>
+      
       
       <LandingProductFeature
         title="Explore the Coast"

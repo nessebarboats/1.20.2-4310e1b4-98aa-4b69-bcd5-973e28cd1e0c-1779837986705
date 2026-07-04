252799ed872671da28b0c5e91aca568a6cd04977
nessebarboats <nessebarboats@gmail.com>
2026-07-04 13:47:06 +0300

    Create route.ts

---
+++ app/api/test/route.ts
@@ -0,0 +1,13 @@
+import { supabase } from "@/lib/supabase";
+
+export async function GET() {
+  const { data, error } = await supabase
+    .from("test")
+    .select("*")
+    .limit(1);
+
+  return Response.json({
+    data,
+    error,
+  });
+}

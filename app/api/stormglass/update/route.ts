bc3acccf94e69baaa4964e4ef438c5cf039c58b7
nessebarboats <nessebarboats@gmail.com>
2026-07-11 16:21:49 +0300

    Create route.ts

---
+++ app/api/stormglass/update/route.ts
@@ -0,0 +1,33 @@
+import { NextResponse } from "next/server";
+import { supabase } from "@/lib/supabase";
+
+export async function POST() {
+  const response = await fetch(
+    `https://api.stormglass.io/v2/weather/point?lat=42.66001330172242&lng=27.74372845766975`,
+    {
+      headers: {
+        Authorization: process.env.918deae0-7b2e-11f1-bcd5-0242ac120004-918decd4-7b2e-11f1-bcd5-0242ac120004,
+      },
+    }
+  );
+
+  const json = await response.json();
+
+  // parse json...
+
+  const { error } = await supabase
+    .from("stormglass")
+    .insert({
+      wave_height: json.hours[0].waveHeight.noaa,
+      wind_speed: json.hours[0].windSpeed.noaa,
+      created_at: new Date().toISOString(),
+    });
+
+  if (error) {
+    return NextResponse.json(error, { status: 500 });
+  }
+
+  return NextResponse.json({
+    success: true,
+  });
+}

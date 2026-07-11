d473b477fe928f2a545d0842d0e9b4535f905660
nessebarboats <nessebarboats@gmail.com>
2026-07-11 17:47:18 +0300

    Update route.ts

--- app/api/stormglass/update/route.ts
+++ app/api/stormglass/update/route.ts
@@ -2,5 +2,8 @@ import { NextResponse } from "next/server";
 import { supabase } from "@/lib/supabase";
 
 export async function GET() {
- 
+ return NextResponse.json({
+    success: true,
+    message: "Stormglass API route is working",
+  });
 }

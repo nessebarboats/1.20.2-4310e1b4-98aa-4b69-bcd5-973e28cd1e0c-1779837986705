248bbbf1cb50630f14f474fdd3c3ed044599cd68
nessebarboats <nessebarboats@gmail.com>
2026-08-20 01:00:39 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -30,19 +30,19 @@ import { supabase } from "@/lib/supabase";
 import Image from "next/image" 
 
 import MapboxMap, { MapMarker } from '@/components/MapboxMap';
-const locations: MapMarker[] = [
+/*const locations: MapMarker[] = [
   id: 1;
   longitude: 42.659820;
   latitude: 27.743707;
   label?: "LABEL";      // shown in the popup
   color?: "#0ea5e9";       // marker color, e.g. "#0ea5e9"
-],
+],*/
 
 
 const locations: MapMarker[] = [
-  { id: 1, longitude: 122.4194, latitude: 37.7749, label: 'San Francisco HQ' },
-  { id: 2, longitude: 74.006, latitude: 40.7128, label: 'New York Office' },
-  { id: 3, longitude: 0.1276, latitude: 51.5074, label: 'London Office' },
+  { id: 1, longitude: 122.4194, latitude: 37.7749, label: 'San Francisco HQ', color: '#0ea5e9' },
+  { id: 2, longitude: 74.006, latitude: 40.7128, label: 'New York Office', color: '#0ea5e9' },
+  { id: 3, longitude: 0.1276, latitude: 51.5074, label: 'London Office', color: '#0ea5e9' },
 ];
 
 
